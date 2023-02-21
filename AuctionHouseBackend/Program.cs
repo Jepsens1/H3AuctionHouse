362288a8e180649c12af38c2696e96ebc6ba098e
PatrickJessen <patrick-j1@hotmail.dk>
2023-02-21 13:18:48 +0100

    Created login system

--- AuctionHouseBackend/Program.cs
+++ AuctionHouseBackend/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace AuctionHouseBackend
+﻿using AuctionHouseBackend.Database;
+using AuctionHouseBackend.Managers;
+
+namespace AuctionHouseBackend
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LoginManager login = new LoginManager(new DatabaseLogin("Server=PJJ-P15S-2022\\SQLEXPRESS;Database=AuctionHouse;Trusted_Connection=True;"));
+
+            while (true)
+            {
+
+                Console.WriteLine("1. Create account\n2. Login");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    Console.WriteLine("add user");
+                    Console.Read();
+                    if (!login.CreateAccount(new Models.UserModel("Patrick", "Jessen", "PJJ", "pjj@mail.dk", "test1234")))
+                    {
+                        Console.WriteLine("Username already exists");
+                        Console.Read();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Account created");
+                        Console.Read();
+                    }
+                }
+                else if (input == 2)
+                {
+                    Console.WriteLine("Login");
+                    Console.WriteLine("Username: ");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("Password: ");
+                    string password = Console.ReadLine();
+                    if (login.Login(username, password))
+                    {
+                        Console.WriteLine("Sucessfully logged in!");
+                        Console.Read();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to login");
+                        Console.Read();
+                    }
+                }
+
+            }
         }
     }
 }
