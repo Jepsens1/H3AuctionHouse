92554fccf736d433696aab0990f980b835c31aad
PatrickJessen <patrick-j1@hotmail.dk>
2023-02-22 12:54:27 +0100

    Added functionality to create and get products

---
+++ AuctionHouseBackend/Managers/AuctionProductManager.cs
@@ -0,0 +1,40 @@
+﻿using AuctionHouseBackend.Database;
+using AuctionHouseBackend.Interfaces;
+using AuctionHouseBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionHouseBackend.Managers
+{
+    public class AuctionProductManager : IProductManager<AuctionProductModel>
+    {
+        private DatabaseAuctionProduct databaseAuctionProduct;
+        public AuctionProductManager(DatabaseAuctionProduct databaseAuctionProduct) 
+        { 
+            this.databaseAuctionProduct = databaseAuctionProduct;
+        }
+
+        public List<AuctionProductModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(AuctionProductModel product)
+        {
+            return databaseAuctionProduct.Create(product);
+        }
+
+        public List<AuctionProductModel> GetUserProducts(int userId)
+        {
+            return databaseAuctionProduct.GetUserProducts(userId);
+        }
+
+        List<AuctionProductModel> IProductManager<AuctionProductModel>.GetProduct(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
