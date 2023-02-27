fc908652f024410898f75ff8585e84417f7e2838
Phillip Jepsen <jepsens123@outlook.dk>
2023-02-27 12:37:21 +0100

    Minor error handling, and input field requirements on register

--- H3AuctionHouse/Pages/Register.cshtml.cs
+++ H3AuctionHouse/Pages/Register.cshtml.cs
@@ -10,13 +10,17 @@ namespace H3AuctionHouse.Pages
     public class RegisterModel : PageModel
     {
         [Required]
-        [BindProperty]
+        [BindProperty, StringLength(20, MinimumLength = 5, ErrorMessage = "Username no longer than 20 and less than 5")]
         public string Username { get; set; }
 
         [Required]
-        [BindProperty]
+        [BindProperty, StringLength(20, MinimumLength = 5, ErrorMessage = "Password no longer than 16 and less than 8")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        //Used to compare Password property and ConfirmPassword property
+        [BindProperty, Required, Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
         [Required]
         [BindProperty]
         [DataType(DataType.EmailAddress)]
